b32b30c5dfb777cae1d61152f6079c7c42b03bcd
Emiliano2902 <153856758+Emiliano2902@users.noreply.github.com>
2023-12-15 10:37:25 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,22 @@
 <html>
 <head>
-    <title>Problema</title>
+    <title>Mi Programa</title>
 </head>
 <body>
-    <?php
-        function retornarColorHexa($amarillo, $rosa, $blanco)
-        {
-            $color = sprintf("#%02X%02X%02X", $amarillo, $rosa, $blanco);
-            return $color;
-        }
+<?php
+function retornarColorHexa($amarillo, $rosa, $blanco) {
+    $color = sprintf("#%02X%02X%02X", $amarrillo, $rosa, $blanco);
+    return $color;
+}
 
-        // Modifica esto con tu nombre y número de control
-        $mi_nombre = "emiliano";
-        $mi_numero_de_control = "21091094";
-
-        $color_nombre = retornarColorHexa(255, 0, 0);  // amarillo
-        $color_numero_de_control = retornarColorHexa(0, 0, 255);  // rosa
-    ?>
-    
-    <table>
-        <tr>
-            <td bgcolor="<?php echo $color_nombre; ?>">Nombres</td>
-            <td bgcolor="<?php echo retornarColorHexa(0, 255, 0); ?>">Apellidos</td>
-            <td bgcolor="<?php echo $color_numero_de_control; ?>">No. De control</td>
-        </tr>
-    </table>
+$nombreColor = retornarColorHexa(255, 0, 0);  // Red background
+$numeroColor = retornarColorHexa(0, 255, 0);  // Green background
+?>
+<table>
+    <tr>
+        <td bgcolor="<?php echo $nombreColor ?>">Carlos Emiliano Salgado Plata</td>
+        <td bgcolor="<?php echo $numeroColor ?>">21091094</td>
+    </tr>
+</table>
 </body>
 </html>
